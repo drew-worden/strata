25539cedb231471ebc23f82f00a7ecc0a38f56f1
drew-worden <drew.p.worden@gmail.com>
2024-06-15 20:11:33 -0400

    Padded vocab size to be n**2.

--- src/arch/config.py
+++ src/arch/config.py
@@ -8,7 +8,7 @@ class StrataConfig:
     """Configuration for the Strata model."""
 
     block_size: int = 1024  # max sequence length
-    vocabulary_size: int = 50257  # number of unique tokens
+    vocabulary_size: int = 50304  # number of unique tokens
     num_layers: int = 24  # number of layers in the model
     num_head: int = 24  # number of heads in the model
     num_embedding_dim: int = 768  # embedding dimension
